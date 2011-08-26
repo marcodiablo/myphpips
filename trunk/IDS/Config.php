bc349947ebb47bcdad4d3687769e7d025eceb7c4
adnan.shukor@gmail.com <adnan.shukor@gmail.com@ff9bcd6b-dfae-2af5-6a1e-96eb6b053135>
2011-08-26 04:02:03 +0000

    Initial commit

---
+++ trunk/IDS/Config.php
@@ -0,0 +1,16 @@
+<?php
+//========================================
+//		CONFIGURATION
+//========================================
+
+$MyPHPIPS_blocking_mode     = 'normal';
+// 'normal'	- for SQLi,RFI,LFI,RCE
+// 'aggressive'	- for SQLi,RFI,LFI,RCE,XSS,CSRF (but would cause false positive in this version)
+
+$MyPHPIPS_cache_folder      = 'cache';
+    
+$MyPHPIPS_error_message     = '<h1>HTTP Error 403 - Forbidden</h1>';
+
+$MyPHPIPS_impact_threshold  = 5;
+
+?>
